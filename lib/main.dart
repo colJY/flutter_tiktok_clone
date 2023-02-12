7d4ac4a9ffaa18249dee45195cfa8b0c93420423
colJY <ljy162518@gmail.com>
2023-02-12 22:22:02 +0900

    username Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 
+import 'constants/sizes.dart';
+
 void main() {
   runApp(const TikTokApp());
 }
@@ -13,7 +15,19 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
         primaryColor: const Color(0xFFE9435A),
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+          ),
+        ),
       ),
       home: const SignUpScreen(),
     );
